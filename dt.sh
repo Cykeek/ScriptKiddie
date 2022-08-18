3b3801aff8108e84e75df4fc61a07020784fad9a
Cykeek <Cykeek@proton.me>
2022-08-18 10:36:49 +0530

    Added new branches *support for more roms *support for A13 device trees
    Signed-off-by: Cykeek <Cykeek@proton.me>

--- dt.sh
+++ dt.sh
@@ -35,8 +35,12 @@ case $options in
 	echo "|  *cherish(origin)              |";
 	echo "|  *voltage                      |";
 	echo "|  *pixel                        |";
+	echo "|  *kaleidoscope                 |";
+	echo "|  *lineage                      |";
+	echo "|  *ricedroid                    |";
+	echo "|  *blaze                        |";
 	echo "==================================";
-	read -p "Select your branches (1/2/3) :" branch
+	read -p "Select your branches (1/2/3/4/5/6/7) :" branch
 	case $branch in
 		1)echo "";
 		git clone https://github.com/Cykeek/device_realme_RMX1851 device/realme/RMX1851/
@@ -56,13 +60,55 @@ case $options in
 		sleep 1s;
 		echo "Pixel branch synced!!";
 		echo "";;
+		4)echo "";
+		git clone https://github.com/Cykeek/device_realme_RMX1851 -b kaleidoscope device/realme/RMX1851/
+		echo "";
+		sleep 1s;
+		echo "Kaleidoscope branch synced!!";
+		echo "";;
+		5)echo "";
+		git clone https://github.com/Cykeek/device_realme_RMX1851 -b lineage device/realme/RMX1851/
+		echo "";
+		sleep 1s;
+		echo "Lineage branch synced!!";
+		echo "";;
+		6)echo "";
+		git clone https://github.com/Cykeek/device_realme_RMX1851 -b rice device/realme/RMX1851/
+		echo "";
+		sleep 1s;
+		echo "Ricedroid branch synced!!";
+		echo "";;
+		7)echo "";
+		git clone https://github.com/Cykeek/device_realme_RMX1851 -b blaze device/realme/RMX1851/
+		echo "";
+		sleep 1s;
+		echo "Blaze branch synced!!";
+		echo "";;
 	esac
 	sleep 1s;
 	echo "Fecthing device_realme_sdm710-common from Github";
 	echo "";
 	sleep 1s;
-	git clone https://github.com/Cykeek/device_realme_sdm710-common device/realme/sdm710-common/
-	echo "";
+        echo "==================================";
+        echo "|         Branches               |";
+        echo "|  *12.1(origin)                 |";
+        echo "|  *A13                          |";
+        echo "==================================";
+        read -p "Select your branches (1/2) :" common
+        case $common in
+		1)echo "";
+		echo "You Choosed A12 branch";
+		echo "";
+		git clone https://github.com/Cykeek/device_realme_sdm710-common device/realme/sdm710-common/
+		echo "";
+		sleep 1s;;
+		2)echo "";
+		echo "You Choosed A13 branch";
+	        echo "";
+	        git clone https://github.com/Cykeek/device_realme_sdm710-common -b A13 device/realme/sdm710-common/
+	        echo "";
+		sleep 1s;;
+	esac
 	sleep 1s;
 	echo "Fetching vendor_realme_sdm710-common from Github";
 	echo "";
