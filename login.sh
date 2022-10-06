afdc78f9043ea5f12ef714a8b8f22d1003a15794
Cykeek <Cykeek@proton.me>
2022-10-06 10:48:00 +0530

    Login.sh: Miscellaneous changes * Added A list in txt format * Optimize code * Added option to directly read logs (e.g Case Sensitive) * BETA
    Signed-off-by: Cykeek <Cykeek@proton.me>

--- login.sh
+++ login.sh
@@ -11,15 +11,9 @@ echo;
 
 read -p "Enter ur username: " user
 
-Users=("Cykeek","root","karthick","hyper")
-echo $Users >> Users.txt
-
-if grep -Fq "$user" "Users.txt"
+if grep -Fq "$user" "list.txt"
 then
 	echo "users matched";
-	sleep 2s;
-	echo "cleaning Files..";
-	rm -rf users.txt;
 	sleep 2s;
 	echo;
 	echo "$user logged in " >> logs.txt;
@@ -27,14 +21,43 @@ then
 	echo "================" >> logs.txt;
 	echo "!!Welcome to Hyper server!!";
 	sleep 1s;
+	echo;
+	read -p "Do u want to read logs?(Y/N): " options
+	case $options in 
+	Y)echo"";
+	echo "Reading Logs.txt...";
+	sleep 2s;
+	echo "Extracting Details...";
+	sleep 1s;
+	cat logs.txt;
+	echo "Exiting!!";
+	sleep 2s;
+	exit;;
+	N)echo "";
+	echo "Exiting!!";
+	exit;;
+esac
 else
 	echo "intruder found";
 	sleep 2s;
-        echo "cleaning Files..";
-        rm -rf users.txt;
 	echo "Intruder name is: $user" >> intruder.txt;
 	date >> intruder.txt
 	echo "=======================" >> intruder.txt;
 	echo;
-	exit
+        read -p "Do u want to know whos is the real Intruder?(Y/N): " intruder
+        case $intruder in
+        Y)echo"";
+        echo "Reading intruder.txt...";
+        sleep 2s;
+        echo "Extracting Details...";
+        sleep 1s;
+        cat intruder.txt;
+	echo "Exiting!!";
+	sleep 2s;
+	exit;;
+        N)echo"";
+        echo "Exiting!!"
+	sleep 2s;
+	exit;;
+esac
 fi
